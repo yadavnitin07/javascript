8de2b3423d8d27b9ab41a4a851769d2be058e82e
nitin kumar yadav <ny14381608@gmail.com>
2024-02-06 14:38:05 +0530

    first commit

---
+++ QR code/mcstan.js
@@ -0,0 +1,19 @@
+/*let Qrcode = require ( 'qrcode');
+
+Qrcode.toFile('./qr.png' , 'chal chal ke dikha skirrrr purrr '
+
+{
+    color:{
+        dark: '#00F' ,
+
+        light: '#0000',
+    },
+},
+function (err){
+    if(err) throw err;
+    console.log('done');
+}
+
+);
+*/
+console.log("hello world");
---
+++ my first js file/index.js
@@ -0,0 +1,51 @@
+/*
+  1. ways to print javascript
+  console.log("HELLO NITIN");
+  alert("me");
+  document.write("this is a document");
+*/
+
+// 2. javasript console API (APPLICATION PROGRAMMING INTERFACE)
+// console.log("HELLO WORLD", 4 + 6, "another log");
+// console.warn("last warning");
+// console.error("last error");
+// console.assert(4 == 4);
+
+// 3. javascript variables
+// what are vaariables? = containers to store data values
+/*
+mast 
+cheez 
+hai
+*/
+/*
+var number1 = 45;
+var number2 = 44;
+console.log ( number1 + number2);
+
+// 4. Data types in javascript
+// You can write string in both single and double quotes ("") ('')
+
+// number datatypes
+var num1 = 48;
+var num2 = 58; 
+
+// string data types 
+var str1 = "This is a string ";
+var str2 = 'This is also a string';
+*/
+// objects datatypes
+    var marks = {
+    nitin: 99,
+    sahani: 10,
+    aman: 50, 
+    ankit: 80,
+ }
+
+ console.log(marks);
+
+// Booleans 
+ var a = true;  
+ var b = false; 
+console.log[a,b];
+
